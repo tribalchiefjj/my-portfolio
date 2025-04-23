e874cf3a7198efa93837d6e4a7a18da17a043a6a
jafar <tribalchiefjj@gmail.com>
2025-04-23 11:47:31 +0300

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/desktop_pc/**/*",
+          dest: "desktop_pc",
+        },
+        {
+          src: "public/planet/**/*",
+          dest: "planet",
+        },
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("three")) return "three"; // Separate Three.js
-            if (id.includes("framer-motion")) return "framer"; // Separate animations
-            return "vendor"; // Separate all other dependencies
+            if (id.includes("three")) return "three";
+            if (id.includes("framer-motion")) return "framer";
+            return "vendor";
           }
         },
       },
